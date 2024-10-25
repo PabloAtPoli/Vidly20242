880030d77c417f2d100519ff956d534c24a90d5d
Pablo Ortiz <paortiz@elpoli.edu.co>
2024-10-25 08:55:08 -0500

    Index page created with bootstrap

--- Movies/views.py
+++ Movies/views.py
@@ -1,7 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
     """The home page for Movies"""
-    return HttpResponse("Hello, world. You're at the Movies index.")
+
+    movies = Movie.objects.all()
+    # output = ', '.join([m.title for m in movies])
+    return render(request, 'movies/index.html', {'movies': movies})
 

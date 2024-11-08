a6a9de7b9d6bbbdfcf9b5fe7c582fd85830337b5
Pablo Ortiz <paortiz@elpoli.edu.co>
2024-11-08 09:42:14 -0500

    API for movies created

--- Movies/views.py
+++ Movies/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 # Create your views here.
 from django.http import HttpResponse
+
 def index(request):
     """The home page for Movies"""
 
     movies = Movie.objects.all()
     # output = ', '.join([m.title for m in movies])
     return render(request, 'movies/index.html', {'movies': movies})
+
+def detail(request, movie_id):
+    """Show a single movie and its details"""
+
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
+
 
--- vidly20242/settings.py
+++ vidly20242/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Movies.apps.MoviesConfig'
+    'Movies.apps.MoviesConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'vidly20242.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Added the path to the templates directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
